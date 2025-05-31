0868c51dd08fc42e41df3560730e21fc8b577522
jitendraprajapatt <72989611+jitendraprajapatt@users.noreply.github.com>
2025-05-31 23:02:45 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-petrolearns.onrender.com/api' || 'http://localhost:5000/api',
+  baseURL: 'https://backend-petrolearns.onrender.com/api' ,
   withCredentials: true,
 });
 
